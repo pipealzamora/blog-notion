5ee22287410c03c6b894c05abf3241f33f5fd821
pipeaalzamora <pipeaalzamora@gmail.com>
2024-08-14 14:55:11 -0400

    style error

--- pages/404.js
+++ pages/404.js
@@ -6,7 +6,7 @@ export default function Page404 () {
 
   return (
     <Container>
-      <h1 className="text-5xl text-black dark:text-white text-center">404</h1>
+      <h1 className="text-5xl text-black dark:text-white text-center">Esta pagina no esta dispinoble!</h1>
       <p className="text-xl text-gray-600 dark:text-gray-300 text-center">{locale.PAGE.ERROR_404.MESSAGE}</p>
     </Container>
   )
